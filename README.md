51685861b306587e57e0d0f1ec0a074d4698f67e
Ritik <ritikraushan160@gmail.com>
2024-05-08 19:57:38 +0530

    commit

--- README.md
+++ README.md
@@ -1,70 +1,19 @@
-# Getting Started with Create React App
+# Image Generator
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project is a web application built with React that allows users to generate images based on their text input.
+Suppose you want to generate any image on the basis of your text description so you just need to input the text and AI will generate the image and will render to you. Doesn't it sound cool? 😎
 
-## Available Scripts
+Just try this project to generate your favourite image and save it to your gallery.. 🚀
 
-In the project directory, you can run:
+## Features
 
-### `npm start`
+- Users can enter text to generate AI images.
+- Generated images are displayed on the website.
+- Users can download the generated images.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Technologies Used
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+- React: A JavaScript library for building user interfaces.
+- Tailwind: Styling the UI
+- Hugging Face API for generating Model
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/Myimage.js
+++ src/Myimage.js
@@ -1,9 +1,10 @@
 import React, { createRef, useState } from 'react';
 import { createFileName, useScreenshot } from 'use-react-screenshot';
-
+import {MutatingDots } from 'react-loader-spinner'
 function Myimage() {
     const [input, setInput] = useState('');
     const [imageUrl, setImageUrl] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     const passInput = (event) => {
         setInput(event.target.value);
@@ -11,6 +12,7 @@ function Myimage() {
 
     const token = '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';
     const generateImage = async () => {
+        setLoading(true); // Set loading state to true
         try {
             const response = await fetch(
                 "https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
@@ -33,21 +35,23 @@ function Myimage() {
             setImageUrl(url);
         } catch (error) {
             console.error("Error fetching image:", error);
+        } finally {
+            setLoading(false); // Set loading state to false when done
         }
     };
 
 
     const ref = createRef(null);
     const [image, takeScreenShot] = useScreenshot({
-    type: "image/jpeg",
-    quality: 1.0
-  });
+        type: "image/jpeg",
+        quality: 1.0
+    });
 
     const download = (image, { name = input, extension = "jpg" } = {}) => {
-      const a = document.createElement("a");
-      a.href = image;
-      a.download = createFileName(extension, name);
-      a.click();
+        const a = document.createElement("a");
+        a.href = image;
+        a.download = createFileName(extension, name);
+        a.click();
     };
 
     const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
@@ -55,46 +59,64 @@ function Myimage() {
 
     return (
         <>
-         <div>
-            <form class="max-w-md mx-auto m-10">   
-                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div class="relative">
-                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>
+            <div>
+                <form class="max-w-md mx-auto m-10">   
+                    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </div>
+                        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-600 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-600 dark:focus:border-gray-900" placeholder="Search image for" 
+                            value={input}
+                            onChange={passInput}
+                            required />
+                        <button type="submit"  class="text-white absolute end-2.5 bottom-2.5 bg-gray-600 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-400 dark:focus:ring-gray-600"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                generateImage();
+                                setInput('');
+                            }}>Generate Image</button>
+                    </div>
+                </form>
+            </div>
+
+            {loading ? ( 
+                <div className='flex items-center justify-center'>
+                <div className='flex flex-col items-center justify-center w-80 h-80'>
+                <MutatingDots
+                    visible={true}
+                    height="100"
+                    width="100"
+                    color="#A0DEFF"
+                    secondaryColor="#A0DEFF"
+                     radius="12.5"
+                    ariaLabel="mutating-dots-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    />
                 </div>
-                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-600 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-600 dark:focus:border-gray-900" placeholder="Search image for" 
-                value={input}
-                onChange={passInput}
-                required />
-                <button type="submit"  class="text-white absolute end-2.5 bottom-2.5 bg-gray-600 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-400 dark:focus:ring-gray-600"
-                onClick={(e) => {
-                e.preventDefault();
-                generateImage();
-                setInput('');
-            }}>Generate Image</button>
-        </div>
-    </form>
-    </div>
-
-    {imageUrl && (
-    <div className='flex flex-col items-center'>
-        <div className='flex justify-center'>
-            <img ref={ref} src={imageUrl} className='w-80 h-80' alt="AI Pic" />
-        </div>
-        <div className="mt-4">
-            <button type="submit" className="text-white bg-gray-600 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-400 dark:focus:ring-gray-600"
-                onClick={(e) => {
-                    e.preventDefault();
-                    downloadScreenshot();
-                }}>
-                Download
-            </button>
-        </div>
-    </div>
-)}
-
+            </div>
+            
+            ) : (
+                imageUrl && (
+                    <div className='flex flex-col items-center'>
+                        <div className='flex justify-center'>
+                            <img ref={ref} src={imageUrl} className='w-80 h-80' alt="AI Pic" />
+                        </div>
+                        <div className="mt-4">
+                            <button type="submit" className="text-white bg-gray-600 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-400 dark:focus:ring-gray-600"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    downloadScreenshot();
+                                }}>
+                                Download
+                            </button>
+                        </div>
+                    </div>
+                )
+            )}
         </>
     );
 }
